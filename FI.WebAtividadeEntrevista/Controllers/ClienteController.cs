64ad6550d5eeeb58aa010fea6c5430c70f8a1d47
Moreira <moreiragusta28@gmail.com>
2026-05-24 17:11:22 -0300

    feat(controller): Adiciona actions para gerenciar beneficiários

--- FI.WebAtividadeEntrevista/Controllers/ClienteController.cs
+++ FI.WebAtividadeEntrevista/Controllers/ClienteController.cs
@@ -1,11 +1,12 @@
 ﻿using FI.AtividadeEntrevista.BLL;
-using WebAtividadeEntrevista.Models;
+using FI.AtividadeEntrevista.DML;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using FI.AtividadeEntrevista.DML;
+using System.Web.UI.WebControls;
+using WebAtividadeEntrevista.Models;
 
 namespace WebAtividadeEntrevista.Controllers
 {
@@ -158,5 +159,128 @@ namespace WebAtividadeEntrevista.Controllers
                 return Json(new { Result = "ERROR", Message = ex.Message });
             }
         }
+
+        #region Beneficiários
+
+        /// <summary>
+        /// Lista beneficiários de um cliente
+        /// </summary>
+        [HttpGet]
+        public JsonResult ListarBeneficiarios(long idCliente)
+        {
+            try
+            {
+                BoBeneficiario bo = new BoBeneficiario();
+                var beneficiarios = bo.ListarPorCliente(idCliente);
+
+                var lista = beneficiarios.Select(b => new
+                {
+                    Id = b.Id,
+                    CPF = b.CPF,
+                    Nome = b.Nome,
+                    IdCliente = b.IdCliente
+                }).ToList();
+
+                return Json(lista, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                Response.StatusCode = 500;
+                return Json(new { erro = ex.Message }, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        /// <summary>
+        /// Inclui um novo beneficiário
+        /// </summary>
+        [HttpPost]
+        public JsonResult IncluirBeneficiario(BeneficiarioModel model)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    var erros = (from item in ModelState.Values
+                                 from error in item.Errors
+                                 select error.ErrorMessage).ToList();
+
+                    Response.StatusCode = 400;
+                    return Json(string.Join(", ", erros));
+                }
+
+                BoBeneficiario bo = new BoBeneficiario();
+                long id = bo.Incluir(new Beneficiario
+                {
+                    CPF = model.CPF,
+                    Nome = model.Nome,
+                    IdCliente = model.IdCliente
+                });
+
+                return Json(new { sucesso = true, id = id, mensagem = "Beneficiário incluído com sucesso" });
+            }
+            catch (Exception ex)
+            {
+                Response.StatusCode = 400;
+                return Json(new { sucesso = false, mensagem = ex.Message });
+            }
+        }
+
+        /// <summary>
+        /// Altera um beneficiário existente
+        /// </summary>
+        [HttpPost]
+        public JsonResult AlterarBeneficiario(BeneficiarioModel model)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    var erros = (from item in ModelState.Values
+                                 from error in item.Errors
+                                 select error.ErrorMessage).ToList();
+
+                    Response.StatusCode = 400;
+                    return Json(string.Join(", ", erros));
+                }
+
+                BoBeneficiario bo = new BoBeneficiario();
+                bo.Alterar(new Beneficiario
+                {
+                    Id = model.Id,
+                    CPF = model.CPF,
+                    Nome = model.Nome,
+                    IdCliente = model.IdCliente
+                });
+
+                return Json(new { sucesso = true, mensagem = "Beneficiário alterado com sucesso" });
+            }
+            catch (Exception ex)
+            {
+                Response.StatusCode = 400;
+                return Json(new { sucesso = false, mensagem = ex.Message });
+            }
+        }
+
+        /// <summary>
+        /// Exclui um beneficiário
+        /// </summary>
+        [HttpPost]
+        public JsonResult ExcluirBeneficiario(long id)
+        {
+            try
+            {
+                BoBeneficiario bo = new BoBeneficiario();
+                bo.Excluir(id);
+
+                return Json(new { sucesso = true, mensagem = "Beneficiário excluído com sucesso" });
+            }
+            catch (Exception ex)
+            {
+                Response.StatusCode = 400;
+                return Json(new { sucesso = false, mensagem = ex.Message });
+            }
+        }
+
+        #endregion
     }
 }
