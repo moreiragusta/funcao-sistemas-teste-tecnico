b68f66a508225993336f97d83b329f795e32c0fd
Moreira <moreiragusta28@gmail.com>
2026-05-24 16:59:22 -0300

    feat(domain): Cria entidade Beneficiario

---
+++ FI.AtividadeEntrevista/DML/Beneficiario.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FI.AtividadeEntrevista.DML
+{
+    /// <summary>
+    /// Classe de Beneficiário
+    /// </summary>
+    public class Beneficiario
+    {
+        /// <summary>
+        /// ID do beneficiário
+        /// </summary>
+        public long Id { get; set; }
+
+        /// <summary>
+        /// CPF do beneficiário (formato: 999.999.999-99)
+        /// </summary>
+        public string CPF { get; set; }
+
+        /// <summary>
+        /// Nome do beneficiário
+        /// </summary>
+        public string Nome { get; set; }
+
+        /// <summary>
+        /// ID do cliente (Foreign Key)
+        /// </summary>
+        public long IdCliente { get; set; }
+    }
+}
